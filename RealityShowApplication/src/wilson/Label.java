4cfe295a1fabe92c871b6124edfd15f3ada56325
Kieran Wilson <kwhockey10@gmail.com>
2015-09-24 13:54:50 -0400

    Added class Label to format

---
+++ RealityShowApplication/src/wilson/Label.java
@@ -0,0 +1,34 @@
+package wilson;
+
+public class Label {
+
+	private ContestantInformation ci;
+	private String Output;
+	
+	public Label() {
+		
+		
+	}
+	private void formatName(){
+    String lastName = ci.getLastName();
+    String firstName = ci.getName();
+    
+	}
+	private void formatAddress(){
+    String streetNumber = ci.getStreetNumber();
+    String streetName = ci.getStreetAddress();
+    String city = ci.getCity();
+		
+	}
+	private void formatNumber(){
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+}
