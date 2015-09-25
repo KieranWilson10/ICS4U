d45fce9fba8382c43134b574a19b3b0bb8d73562
Kieran Wilson <kwhockey10@gmail.com>
2015-09-25 13:36:33 -0400

    Finished Label class and added Province class

--- RealityShowApplication/src/wilson/Label.java
+++ RealityShowApplication/src/wilson/Label.java
@@ -1,31 +1,78 @@
-package wilson;
 
+package wilson;
+ /**
+  * Label class formats the information to be outputted in the main
+  * @author Kieran Wilson
+  *
+  */
 public class Label {
 
-	private ContestantInformation ci;
-	private String Output;
+	ContestantInformation ci;
+	String formatOutput;
+	String lastName;
+	String firstName;
+	String streetNumber;
+	String streetName;
+	String city;
+    String province;
+    String postalCode;
+    String phoneNumber;
 	
+	private String nameOutput;
+	private String addressOutput;
+	private String phoneNumberOutput;
 	public Label() {
 		
 		
 	}
-	private void formatName(){
-    String lastName = ci.getLastName();
-    String firstName = ci.getName();
+	/**
+	 * 
+	 * @return formatName
+	 */
+	private String formatName(){
+    lastName = ci.getLastName();
+    firstName = ci.getName();
     
+    nameOutput = lastName + ", " + firstName;
+    return nameOutput;
 	}
-	private void formatAddress(){
-    String streetNumber = ci.getStreetNumber();
-    String streetName = ci.getStreetAddress();
-    String city = ci.getCity();
+	/**
+	 * 
+	 * @return formatAddress
+	 */
+	private String formatAddress(){
+    streetNumber = ci.getStreetNumber();
+    streetName = ci.getStreetAddress();
+    city = ci.getCity();
+    province = ci.getProvince();
+    postalCode = ci.getPostalCode();
+    
+    addressOutput = streetNumber + " " + streetName + " \n" + city + " ," + province + " \n" + postalCode;
+    return addressOutput;
+    
 		
 	}
-	private void formatNumber(){
+	/**
+	 * 
+	 * @return formatNumber
+	 */
+	private String formatNumber(){
+		phoneNumber = ci.getPhoneNumber();
 		
-		
+		phoneNumberOutput = phoneNumber;
+		return phoneNumberOutput;
 	}
 	
+	/**
+	 * @return formatOutput
+	 */
+	public String toString(){	
+		
+		formatOutput = formatName() + "\n" + formatAddress() + "\n" + formatNumber();
+		
+		return formatOutput;		
 	
+	}
 	
 	
 	
---
+++ RealityShowApplication/src/wilson/Province.java
@@ -0,0 +1,6 @@
+package wilson;
+
+public enum Province {
+
+	AB, BC , MB , NB , NL, NS , NT , NU , ON , PE , QC , SK , YT
+}
