35cbe0f39a0b9560f5ecb740381c904891d34e79
Kieran Wilson <kwhockey10@gmail.com>
2015-09-28 13:41:35 -0400

    Added main method

---
+++ RealityShowApplication/src/wilson/main.java
@@ -0,0 +1,50 @@
+/**
+ * 
+ */
+package wilson;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ * @author Kieran Wilson
+ *
+ */
+public class main {
+
+	/**
+	 * 
+	 */
+	public main() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		ContestantInformation contestant1 = null;
+		ContestantInformation contestant2 = null;
+		
+		Scanner Scanner = new Scanner(System.in);
+	
+	ArrayList<ContestantInformation> contestant = new ArrayList<ContestantInformation>();
+    
+	
+	contestant.add(contestant1);
+	contestant.add(contestant2);
+	
+	System.out.println("Please enter your first name.");
+    
+	
+
+		
+
+	}
+	
+	
+	
+	
+	
+
+}
