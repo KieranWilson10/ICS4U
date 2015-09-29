d29eadcd8841eb58af0d00ac3fac0d54bc0f7fe7
Kieran Wilson <kwhockey10@gmail.com>
2015-09-29 13:42:31 -0400

    Added user input and contestant1 value input

--- RealityShowApplication/src/wilson/main.java
+++ RealityShowApplication/src/wilson/main.java
@@ -2,6 +2,7 @@
  * 
  */
 package wilson;
+
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -22,29 +23,127 @@ public class main {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		
-		ContestantInformation contestant1 = null;
-		ContestantInformation contestant2 = null;
-		
-		Scanner Scanner = new Scanner(System.in);
-	
-	ArrayList<ContestantInformation> contestant = new ArrayList<ContestantInformation>();
-    
-	
-	contestant.add(contestant1);
-	contestant.add(contestant2);
-	
-	System.out.println("Please enter your first name.");
-    
-	
 
+		boolean flag = false;
+		ContestantInformation contestant1 = new ContestantInformation();
+		Scanner Scanner = new Scanner(System.in);	
+	
 		
+		do {
+			try {
+				flag = false;
+				System.out.println("Please enter your first name.");
+				String firstName = Scanner.nextLine();
+
+				contestant1.setName(firstName);
+
+			} catch (InvalidInputException e) {
+				System.out.println(e.getMessage());
+				flag = true;
+			}
+		} while (flag);
+		
+		do {
+			try {
+				flag = false;
+				System.out.println("Please enter your last name.");
+				String lastName = Scanner.nextLine();
+
+				contestant1.setName(lastName);
+
+			} catch (InvalidInputException e) {
+				System.out.println(e.getMessage());
+				flag = true;
+			}
+		} while (flag);
+		
+		do {
+			try {
+				flag = false;
+				System.out.println("Please enter your street number.");
+				String streetNumber = Scanner.nextLine();
+
+				contestant1.setName(streetNumber);
+
+			} catch (InvalidInputException e) {
+				System.out.println(e.getMessage());
+				flag = true;
+			}
+		} while (flag);
+		
+		do {
+			try {
+				flag = false;
+				System.out.println("Please enter your street address.");
+				String streetAddress = Scanner.nextLine();
+
+				contestant1.setName(streetAddress);
+
+			} catch (InvalidInputException e) {
+				System.out.println(e.getMessage());
+				flag = true;
+			}
+		} while (flag);
+		
+		do {
+			try {
+				flag = false;
+
+				System.out.println("Please enter your city.");
+				String city = Scanner.nextLine();
+
+				contestant1.setName(city);
+
+			} catch (InvalidInputException e) {
+				System.out.println(e.getMessage());
+				flag = true;
+			}
+		} while (flag);
+		
+		do {
+			try {
+				flag = false;
+				System.out.println("Please enter your province.");
+				String province = Scanner.nextLine();
+
+
+				contestant1.setName(province);
+
+			} catch (InvalidInputException e) {
+				System.out.println(e.getMessage());
+				flag = true;
+			}
+		} while (flag);
+		
+		do {
+			try {
+				flag = false;
+				System.out.println("Please enter your postal code.");
+				String postalCode = Scanner.nextLine();
+
+				contestant1.setName(postalCode);
+
+			} catch (InvalidInputException e) {
+				System.out.println(e.getMessage());
+				flag = true;
+			}
+		} while (flag);
+
+		do {
+			try {
+				flag = false;
+
+				System.out.println("Please enter your phone number.");
+				String phoneNumber = Scanner.nextLine();
+
+				contestant1.setName(phoneNumber);
+
+			} catch (InvalidInputException e) {
+				System.out.println(e.getMessage());
+				flag = true;
+			}
+		} while (flag);
 
 	}
-	
-	
-	
-	
-	
 
 }
