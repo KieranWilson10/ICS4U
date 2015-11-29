03a58bfc4688c7a021067a438fda3e8ab3c67761
KieranWilson10 <kwhockey10@gmail.com>
2015-11-28 19:44:40 -0500

    Merge and Quick sort added

--- SortingPractice/SortingPractice/src/wilson/SorterMain.java
+++ SortingPractice/SortingPractice/src/wilson/SorterMain.java
@@ -1,5 +1,6 @@
 package wilson;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class SorterMain {
@@ -42,10 +43,10 @@ public class SorterMain {
 				printArray(bubbleSort(inputList, Integer.parseInt(size)));
 				break;
 			case "4":
-				quickSort(inputList);
+				printArray(quickSort(inputList));
 				break;
 			case "5":
-				mergeSort(inputList, Integer.parseInt(size));
+				printArray(mergeSort(inputList));
 				break;
 
 			}
@@ -89,10 +90,10 @@ public class SorterMain {
 
 	public static int[] bubbleSort(int[] unsorted, int size) {
 		for (int j = 0; j <= unsorted.length - 1; j++) {
-			for (int i = 0; i <= unsorted.length - 1 - i; i++) {				
-				if (unsorted[i] > unsorted [i+1]) {
-					int temp = unsorted[i+1];
-					unsorted[i+1] = unsorted[i];
+			for (int i = 0; i <= unsorted.length - 1 - i; i++) {
+				if (unsorted[i] > unsorted[i + 1]) {
+					int temp = unsorted[i + 1];
+					unsorted[i + 1] = unsorted[i];
 					unsorted[i] = temp;
 				}
 			}
@@ -101,19 +102,101 @@ public class SorterMain {
 
 	}
 
-	public static void quickSort(int[] unsorted) {
-		
-		
+	public static int[] quickSort(int[] unsorted) {
+		if (unsorted == null || unsorted.length == 0) {
+			return null;
+		}
+		quickSort(unsorted, 0, unsorted.length - 1);
+		return unsorted;
 	}
-	public static void mergeSort(int[] unsorted, int size) {
 
+	public static void quickSort(int[] array, int origLow, int origHigh) {
+
+		int low = origLow;
+		int high = origHigh;
+		int pivot = array[origLow + (origHigh - origLow) / 2];
+
+		while (low <= high) {
+			while (array[low] < pivot) {
+				low++;
+			}
+			while (array[high] > pivot) {
+				high--;
+			}
+			if (low <= high) {
+				int temp = array[low];
+				array[low] = array[high];
+				array[high] = temp;
+				low++;
+				high--;
+			}
+		}
+		if (origLow < high) {
+			quickSort(array, origLow, high);
+		}
+		if (low < origHigh) {
+			quickSort(array, low, origHigh);
+		}
+	}
+	
+	public static int[] mergeSort(int[] array){		
+		mergeSortSplit(array);
+		return array;
+	}
+
+	public static void mergeSortSplit(int[] unsorted) {
+
+		if (unsorted.length > 1) {		
+			
+		int[] array1 = Arrays.copyOfRange(unsorted, 0, unsorted.length / 2);
+		int[] array2 = Arrays.copyOfRange(unsorted, unsorted.length / 2, unsorted.length);
+		mergeSortSplit(array1);
+		mergeSortSplit(array2);
+		merge(unsorted, array1, array2);
+		}		
+
+	}
+
+	public static void merge(int original[], int[] array1, int[] array2) {
+		int totalLength = array1.length + array2.length;
+		int i = 0;
+		int array1Index = 0;
+		int array2Index = 0;
+
+		while (i < totalLength) {
+			if ((array1Index < array1.length) && (array2Index < array2.length)) {
+				if (array1[array1Index] < array2[array2Index]) {
+					original[i] = array1[array1Index];
+					array1Index++;
+					i++;
+				} else {
+					original[i] = array2[array2Index];
+					array2Index++;
+					i++;
+				}
+			} else {
+				if (array1Index >= array1.length) {
+					while (array2Index < array2.length) {
+						original[i] = array2[array2Index];
+						array2Index++;
+						i++;
+					}
+				} else if (array2Index >= array2.length) {
+					while (array1Index < array1.length) {
+						original[i] = array1[array1Index];
+						array1Index++;
+						i++;
+					}
+				}
+			}
+		}
 	}
 
 	public static void printArray(int[] array) {
+
 		for (int i = 0; i < array.length; i++) {
 			System.out.println(array[i]);
 		}
 
 	}
-
 }
---
+++ SortingPractice/SortingPractice/src/wilson/main.java
@@ -0,0 +1,15 @@
+package wilson;
+
+public class main {
+
+	public main() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		int[] list = {1,2,5,3,8};
+		System.out.println(SorterMain.quickSort(list));
+
+	}
+
+}
